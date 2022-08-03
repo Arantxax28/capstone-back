125c03611b92cc6f15b47c008dcc472d66243116
Diana <arantxa.codes@gmail.com>
2022-08-03 09:48:52 -0700

    removed hard-coded information from postgres

--- src/main/java/com/capstone/uzit/product/ProductConfig.java
+++ src/main/java/com/capstone/uzit/product/ProductConfig.java
@@ -15,24 +15,24 @@ public class ProductConfig {
     CommandLineRunner commandLineRunner(
             ProductRepository repository) {
         return args -> {
-            Product cloudPaint = new Product(
-                    "Cloud Paint",
-                    "Glossier",
-                    "Makeup",
-                    20.00,
-                    LocalDate.of(2022, Month.JULY,27),
-                    LocalDate.of(2023, Month.JANUARY,27)
-            );
-            Product airBrow = new Product(
-                    "Air Brow Eyebrow Gel",
-                    "Kosas",
-                    "Makeup",
-                    24.00,
-                    LocalDate.of(2022, Month.MAY,2),
-                    LocalDate.of(2022, Month.NOVEMBER,2)
-            );
+//            Product cloudPaint = new Product(
+//                    "Cloud Paint",
+//                    "Glossier",
+//                    "Makeup",
+//                    20.00,
+//                    LocalDate.of(2022, Month.JULY,27),
+//                    LocalDate.of(2023, Month.JANUARY,27)
+//            );
+//            Product airBrow = new Product(
+//                    "Air Brow Eyebrow Gel",
+//                    "Kosas",
+//                    "Makeup",
+//                    24.00,
+//                    LocalDate.of(2022, Month.MAY,2),
+//                    LocalDate.of(2022, Month.NOVEMBER,2)
+//            );
             repository.saveAll(
-                    List.of(cloudPaint, airBrow)
+                    List.of()
             );
         };
     }
